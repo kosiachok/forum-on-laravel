2bc47139ebb25ce6da06fbd781c4dc9ea390fa1c
kostia <kostiagordienko17@gmail.com>
2021-11-18 17:36:43 +0200

    Installed Vue.js 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+require('bootstrap');
+window.Vue = require('vue').default;
+
+import { createApp } from "vue";
+import App from './components/App.vue';
+
+const app = createApp(App);
+app.mount("#app");
